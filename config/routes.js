e528e176b333af42138f1f8362540ced42b715c0
Akash Maniar <ak429307@dal.ca>
2020-07-07 22:04:56 -0300

    added routes and empty controller file for operations for company Z

--- config/routes.js
+++ config/routes.js
@@ -1,9 +1,18 @@
 module.exports.routes = {
+
+  // These APIs are made for internal use of company Y
   '/': { view: 'pages/homepage' },
   'GET /getParts' : 'PartsController.getParts',
   'GET /addPart' : {view: 'pages/addPart'},
   'POST /addPart' : 'PartsController.addPart',
   'POST /editPartDetail' : 'PartsController.editPartDetail',
   'POST /editPart/:partId' : 'PartsController.editPart',
-  'POST /deletePart/:partId' : 'PartsController.deletePart'
+  'POST /deletePart/:partId' : 'PartsController.deletePart',
+
+  // These are the APIs are made when to be called externally by others. (eg. Company Z)
+  'GET /getParts' : 'PartsExternalController.getParts',
+  'GET /getParts/:partId' : 'PartsExternalController.getParts',
+  'POST /addPart' : 'PartsExternalController.addPart',
+  'PUT /updatePart/:partId' : 'PartsExternalController.updatePart',
+  'DELETE /deletePart/:partId' : 'PartsExternalController.deletePart',
 };
